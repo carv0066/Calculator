c01e65066bbf602dc1087dd11153c2188f802b41
Carlos Carvajal <carv0066@algonquinlive.com>
2024-07-29 23:34:48 -0400

    Finished adding decimals + Clear button

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const calculatorDisplay = document.querySelector(".screen-text");
 const inputBtns = document.querySelectorAll("button");
-const clearBtn = document.querySelector("delete");
+const clearBtn = document.querySelector(".delete");
+const decimalBtn = document.querySelector(".decimal")
 
 function sendNumberValue (number) {
     // If current display value is 0, replace it, if not we want to add number
-    // add reset "C" + ADD decimal
     const displayValue = calculatorDisplay.textContent;
     if(displayValue === "0") {
         calculatorDisplay.textContent = number;
     } else if(displayValue !== "0") {
         calculatorDisplay.textContent += number;
-    }   
+    } 
+    ///Adding a decimal to a number only once.
+    decimalBtn.addEventListener(("click"), () => {
+        //Disable decimal button once the event listener hears one click
+        decimalBtn.disabled = true;
+        console.log("contains decimals")
+    })
 }
+
+//Reset Value when clicking on the clear "C" button
+clearBtn.addEventListener(("click"), () => {
+    //Set the value of the text box back to 0 whenever I click on the clear button
+    calculatorDisplay.textContent = 0;
+})
+
 
 //Loop trough all elements and return value
 inputBtns.forEach((btn) => {
@@ -28,7 +41,12 @@ inputBtns.forEach((btn) => {
         })
     } else if (btn.classList.contains("decimal")) {
         btn.addEventListener(("click"), () => {
-            sendNumberValue()
+            sendNumberValue(btn.value)
         })
     }
 });
+
+
+function operations () {
+
+}
